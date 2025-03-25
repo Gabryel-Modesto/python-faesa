4d339deed1ef921150c272b3f8c102670354fcb7
Alunos Laboratorio <aluno.laboratorio@faesa.br>
2025-03-25 19:06:20 -0300

    Primeira e segunda aula de python pela faesa

---
+++ aula_1/app.py
@@ -0,0 +1,10 @@
+name = input("Escreva seu nome:")
+print('Olá ', name)
+
+
+number1 = int(input("Informe um número: "))
+number2 = int(input("Informe outro número: "))
+
+sum = number1 + number2
+
+print(sum)
